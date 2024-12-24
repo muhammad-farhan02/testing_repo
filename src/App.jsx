ef9f5c97ebf15938c15ed1a44623eb563a189d43
muhammad-farhan02 <zebaishbedding@gmail.com>
2024-12-24 13:48:44 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+function App() {
+
+  return (
+
+     <>   
+  <h1>Zebaish Corner</h1>
+  <div> Bed sheets</div>
+  <div>Blanket</div>
+  <p>jbgujhhbjbkhjvjhbjvjhvhvhjvjhvhjvjhgkbhvhjv</p>
+  <h1>bhbjbjbjb</h1>
+  <div className='mandiv'><h1>Zebaish Corner</h1></div>
+
+  </>
+        
+
+  
+  )
+}
+
+export default App
